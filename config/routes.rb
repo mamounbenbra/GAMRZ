78a95b46377cfa1264d18b852066e6a286ae80c3
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-05-24 15:43:13 +0200

    kdfj

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only [:show, :index] do
-    resources :reviews, only [:new, :create]
-    resources :matches, only [:new :create]
+    resources :reviews, only: [:new, :create]
+    resources :matches, only: [:new :create]
   end
 end
