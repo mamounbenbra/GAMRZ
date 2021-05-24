30604de608cceb54d752b6158122dd8c51416305
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-05-24 15:47:24 +0200

    ouii

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only [:show, :index] do
+  resources :users, only: [:show, :index] do
     resources :reviews, only: [:new, :create]
     resources :matches, only: [:new, :create]
   end
