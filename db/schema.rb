99f81d2693c8c97a452a9e73caa5d4a00a09b2ff
Henrik Duerrfeld <henrik.duerrfeld.20002@gmail.com>
2021-05-25 15:41:23 +0200

    adding stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_123802) do
+ActiveRecord::Schema.define(version: 2021_05_25_130350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_05_25_123802) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "mutual", default: false
+    t.bigint "chatroom_id"
+    t.index ["chatroom_id"], name: "index_matches_on_chatroom_id"
   end
 
   create_table "messages", force: :cascade do |t|
