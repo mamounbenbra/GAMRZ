b48018a341841d49d3aeb0a590382fed8ee2aba3
Henrik Duerrfeld <henrik.duerrfeld.20002@gmail.com>
2021-05-25 17:09:25 +0200

    adding stuff

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,6 @@
 class MatchesController < ApplicationController
-
+    def index
+        @user = User.find(params[:user_id])
+        @matches = Match.all.where(from_user_id: current_user.id)
+    end
 end
