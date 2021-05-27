8c773cf19b2a23aeefaa8c0b2ee676b9d712b8bb
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-05-27 15:38:20 +0200

    filters good

---
+++ app/javascript/packs/btn-hidden.js
@@ -0,0 +1,16 @@
+const btnHidden = () => {
+  const x = document.querySelector("#btn-hidden");
+  const y = document.querySelector("#btn-hidden2");
+  x.addEventListener('click', event => {
+    console.log(event)
+    console.log(y)
+    y.classList.remove("d-none");
+    x.classList.add("d-none");
+  });
+}
+
+
+
+export { btnHidden } ;
+
+
