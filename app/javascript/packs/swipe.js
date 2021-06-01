0a605bab647e7aeb4a99c2431d32270222af7835
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-06-01 10:36:23 +0200

    bugfixed

--- app/javascript/packs/swipe.js
+++ app/javascript/packs/swipe.js
@@ -9,14 +9,14 @@ const swipe = () => {
                         console.log(no)
                         no.click()
 
-                        setTimeout(function () break; }, 1000);
+                        setTimeout(function () {  break; }, 1000);
 
                     case 39:
                         // Key right.
                         var yes = document.getElementById("yes_btn")
                         console.log(yes)
                         yes.click()
-                        setTimeout(function () break; }, 1000);
+                        setTimeout(function () {  break; }, 1000);
                 }
             });
         });
