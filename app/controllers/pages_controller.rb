ab8bf9e5c5562fde69bd112b18fc69c696afd766
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-06-02 10:28:04 +0200

    pb pundit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,14 @@ class PagesController < ApplicationController
   end
 
   def messages?
-    puts "i am playing messages ?"
-    @empty_chatroom = false
-    matches = Match.where(from_user: current_user.id, mutual: true)
-    if matches.any?
-      matches.each do |match|
-        if match.chatroom.messages.none?
-          @empty_chatroom = true
+    if user_signed_in?
+      @empty_chatroom = false
+      matches = Match.where(from_user: current_user.id, mutual: true)
+      if matches.any?
+        matches.each do |match|
+          if match.chatroom.messages.none?
+            @empty_chatroom = true
+          end
         end
       end
     end
